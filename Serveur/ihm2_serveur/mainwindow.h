0455d4469c05992c9268bbbbb080b4261d3738f8
Gomes Fernandes Caty <caty.gomes-fernandes@turing.u-strasbg.fr>
2017-04-13 19:30:49 +0200

    Création répertoire et fichiers côté serveur

---
+++ Serveur/ihm2_serveur/mainwindow.h
@@ -0,0 +1,29 @@
+/***************************************************************************
+ *                                                                         *
+ *   Gomes Fernandes Caty, Hamery Simon                                    *
+ *   L3 Informatique, S6 Printemps                                         *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
